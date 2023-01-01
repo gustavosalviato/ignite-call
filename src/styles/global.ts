b2ad980852915505e2b7cbb568ef8efc4a1ed246
gustavosalviato <gustavosalviato8@gmail.com>
2023-01-01 10:13:10 -0300

    add eslint config to application

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,16 +1,15 @@
 import { globalCss } from '@ignite-ui/react'
 
 export const globalStyles = globalCss({
-    '*': {
-        padding: 0,
-        margin: 0,
-        boxSizing: 'border-box',
-    },
+  '*': {
+    padding: 0,
+    margin: 0,
+    boxSizing: 'border-box',
+  },
 
-    body: {
-        backgroundColor: '$gray900',
-        color: '$gray100',
-        '-webkit-font-smoothing': 'antialiased',
-
-    }
+  body: {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+    '-webkit-font-smoothing': 'antialiased',
+  },
 })
