205137086f38e9e5adab59e7772998c3f7e7fa5e
gustavosalviato <gustavosalviato8@gmail.com>
2023-01-31 08:15:40 -0300

    .

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -77,7 +77,7 @@ export function CalendarStep({ onSelectDate }: CalendarStepProps) {
                 return (
                   <TimePickerItem
                     key={hour}
-                    disabled={availability.availableTimes.includes(hour)}
+                    disabled={!availability.availableTimes.includes(hour)}
                     onClick={() => handleSelectTime(hour)}
                   >
                     {String(hour).padStart(2, '0')}:00h
