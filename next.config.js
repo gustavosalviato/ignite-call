c1e8971dfbecc2cf00fbf95e09131b1fc51b8e21
gustavosalviato <gustavosalviato8@gmail.com>
2023-01-30 21:14:49 -0300

    add next config for page extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  swcMinify: true,
 }
 
 module.exports = nextConfig
